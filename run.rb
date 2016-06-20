776f6cd56fcf9d822d020060cc8d416644271f8f
Milen Petrov <github.com@milen.net>
2016-06-20 14:52:59 +0000

    ip option without /32

--- run.rb
+++ run.rb
@@ -4,7 +4,7 @@
 # system('ruby assignment.rb')
 
 # should give Output #2
-system('ruby assignment.rb --instances 2 --instance-type t2.small --allow-ssh-from 37.17.210.74/32')
-# system('ruby assignment.rb --xx 22 --aa 33')
+system('ruby assignment.rb --instances 2 --instance-type t2.small --allow-ssh-from 37.17.210.75')
 
-  
+# should give some errors
+# system('ruby assignment.rb --xx 22 --aa 33')
